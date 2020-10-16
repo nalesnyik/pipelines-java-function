3110e16312ae490ab81f8f152f6a234374c3e9ae
nalesnyik <58170691+nalesnyik@users.noreply.github.com>
2020-10-16 18:05:39 +0200

    Update Function.java

--- src/main/java/com/functionp/Function.java
+++ src/main/java/com/functionp/Function.java
@@ -28,7 +28,7 @@ public class Function {
         if (name == null) {
             return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Please pass a name on the query string or in the request body").build();
         } else {
-            return request.createResponseBuilder(HttpStatus.OK).body("Tső Babi néni és: " + name).build();
+            return request.createResponseBuilder(HttpStatus.OK).body("Good morning Lord " + name).build();
         }
     }
 }
